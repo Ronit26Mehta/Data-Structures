b04c133b4436ce21b0345976e1e107cecd0c0cae
Ronit Mehta <109467924+Ronit26Mehta@users.noreply.github.com>
2023-10-14 19:05:14 +0530

    fibonacci_search for tutorial 1 done

---
+++ tutorial 1/fibonacci_search.c
@@ -0,0 +1,72 @@
+#include<stdio.h>
+int min(int x,int y);
+int fibonacci_search(int arr[],int x,int n);
+void main(){
+    int arr[100];
+    int n;
+    int val;
+    int index;
+    printf("\nEnter the size of the array:");
+    scanf("%d",&n);
+    printf("\nstart entering the %d number of elements.",n);
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    printf("\nEnter the value to be searched:");
+    scanf("%d",&val);
+    printf("\nthe array to be searched for the value is:");
+    
+    index = fibonacci_search(arr,val,n);
+    if (index>=0)
+    {
+        printf("\nthe element was found at %d index.",index);
+    }
+    else
+    {
+        printf("\nthe element was mot found");
+    }
+}
+int min(int x,int y){
+    return(x<=y)?x:y;
+}
+int fibonacci_search(int arr[],int x,int n){
+    int fibnm2 = 0;
+    int fibm1 = 1;
+    int fib = fibm1 + fibnm2;
+    while (fib<n)
+    {
+        fibnm2 = fibm1;
+        fibm1 = fib;
+        fib = fibnm2 + fibm1;
+    }
+    int offset = -1;
+    while (fib>1)
+    {
+        int i = min(offset+fibnm2,n-1);
+        if (arr[i]<x)
+        {
+            fib = fibm1;
+            fibm1 = fibnm2;
+            fibnm2 = fib - fibm1;
+            offset = i;
+        }
+        else if (arr[i]>x)
+        {
+            fib = fibnm2;
+            fibm1 = fibm1 - fibnm2;
+            fibnm2 = fib - fibm1;
+
+        }
+        else 
+        {
+            return i;
+        }
+    }
+    if (fibm1 && arr[offset+1]==x)
+    {
+        return offset+1;
+    }
+    return -1;   
+    
+}
