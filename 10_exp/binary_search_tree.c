f42e7616d99a441585c66bb91fe35a05d671a920
Ronit Mehta <109467924+Ronit26Mehta@users.noreply.github.com>
2023-11-09 21:23:50 +0530

    Update binary_search_tree.c

--- 10_exp/binary_search_tree.c
+++ 10_exp/binary_search_tree.c
@@ -16,6 +16,11 @@ struct node *findSamllestElement(struct node *);
 struct node *findLargestElement(struct node *);
 int totalNodes(struct node *);
 struct node *deleteTree(struct node *);
+struct node *deleteElement(struct node*,int);
+struct node *mirrorImage(struct node*);
+int totalExternalNodes(struct node*);
+int totalInternalNodes(struct node*);
+int height(struct node *);
 void main(){
     int choice,val;
     struct node *ptr;
@@ -32,7 +37,12 @@ void main(){
         printf("\n6.Find the largest Element");
         printf("\n7. count the total number of nodes");
         printf("\n8.delete The tree");
-        printf("\n9.exit");
+        printf("\n9.delete an element");
+        printf("\n10.mirrorImage of tree");
+        printf("\n11.total internal nodes");
+        printf("\n12.total external nodes");
+        printf("\n13.height of tree");
+        printf("\n.14.Exit");
         scanf("%d",&choice);
         switch (choice)
         {
@@ -64,17 +74,35 @@ void main(){
         case 7:
         printf("\n Total no. of nodes = %d", totalNodes(tree));
         break;
-       case 8:
+        case 8:
          tree = deleteTree(tree);
          break;
-         case 9:
-         printf("\nExiting");
+        case 9:
+         printf("\nEnter val to be deleted:");
+         scanf("%d",&val);
+         tree = deleteElement(tree,val);
+         break;
+         case 10:
+         tree = mirrorImage(tree);
+         printf("\nThe mirror image of tree is created.");
+         break;
+         case 11:
+         printf("\nTotal number of the internal node=%d ",totalInternalNodes(tree));
+         break;
+         case 12:
+         printf("\nTotal number of the external nodes=%d",totalExternalNodes(tree));
+         break;
+         case 13:
+         printf("\nThe height of the tree is = %d",height(tree));
+         break;
+         case 14:
+         printf("\nExiting...");
          break;
          default:
          printf("\nInvalid Choice");
          break;
         }
-    } while (choice!=9);
+    } while (choice!=14);
     
 }
 void create_tree(struct node *tree){
@@ -176,6 +204,117 @@ struct node *deleteTree(struct node *tree){
         deleteTree(tree->left);
         deleteTree(tree->right);
         free(tree);
+    }
+    
+}
+struct node *deleteElement(struct node *tree,int val){
+    struct node *cur,*parent,*suc,*psuc,*ptr;
+    if (tree->left==NULL)
+    {
+        printf("\nTree is Empty");
+        return(tree);
+    }
+    parent = tree;
+    cur = tree->left;
+    while (cur!=NULL && val!=cur->data)
+    {
+        parent = cur;
+        cur = (val<cur->data)?cur->left:cur->right;
+    }
+    if (cur == NULL)
+    {
+        printf("\nThe value to be deleted is not found");
+        return(tree);
+    }
+    if (cur->left==NULL)
+    {
+        ptr=cur->right;
+    }
+    else if (cur->right == NULL)
+    {
+        ptr = cur->right;
+    }
+    else{
+        psuc = cur;
+        cur = cur ->left;
+        while (suc->left!=NULL)
+        {
+            psuc=suc;
+            suc = suc->left;
+        }
+        if (cur==psuc)
+        {
+            suc->left=cur->right;
+        }
+        else{
+            suc->left=cur->left;
+            psuc->left=suc->right;
+            suc->right=cur->right;
+        }
+        
+        ptr = suc;
+    }
+    if (parent->left==cur)
+    {
+        parent->left=ptr;
+    }
+    else{
+        parent->right=ptr;
+    }
+    free(cur);
+    return tree;
+}
+struct node *mirrorImage(struct node *tree){
+    struct node *ptr;
+    if (tree!=NULL)
+    {
+        mirrorImage(tree->left);
+        mirrorImage(tree->right);
+        ptr=tree->left;
+        ptr->left=ptr->right;
+        tree->right=ptr;
+    }
+    
+}
+int totalInternalNodes(struct node *tree){
+    if ((tree == NULL ) || (tree->left==NULL)&& (tree->right==NULL))
+    {
+        return 0;
+    }
+    else{
+        return(totalInternalNodes(tree->left)+totalInternalNodes(tree->right)+1);
+    }
+    
+}
+int totalExternalNodes(struct node *tree){
+    if (tree==NULL)
+    {
+        return 0;
+    }
+    else if((tree->left==NULL) && (tree->right==NULL)){
+        return 1;
+    }
+    else{
+        return (totalExternalNodes(tree->left)+totalExternalNodes(tree->right));
+    }
+}
+int height(struct node *tree){
+    int leftheight,rightheight;
+    if (tree==NULL)
+    {
+        return 0;
+    }
+    else{
+        leftheight = height(tree->left);
+        rightheight = height(tree->right);
+        if (leftheight>rightheight)
+        {
+            return (leftheight + 1);
+        }
+        else{
+            return (rightheight + 1);
+        }
+        
     }
     
 }
